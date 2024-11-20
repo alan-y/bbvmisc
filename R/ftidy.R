7bdde60b85d4ffe23ae27c175c8c7cee1687f47f
Alan Yeung <alan.yeung@phs.scot>
2024-11-20 10:43:34 +0000

    ftidy drop_intercept argument

--- R/ftidy.R
+++ R/ftidy.R
@@ -3,13 +3,14 @@
 #'
 #' @param model Regression model.
 #' @param exp Option to exponentiate coefficients.
+#' @param drop_intercept Option to drop intercept term from the output table.
 #' @return Regression model table with confidence intervals.
 #' @export
 #' @examples
 #' \dontrun{
 #' ftidy(mod)
 #' }
-ftidy <- function(model, exp = TRUE) {
+ftidy <- function(model, exp = TRUE, drop_intercept = FALSE) {
   model_tab <- broom::tidy(model, exponentiate = exp)
   mod_ci <- tibble::as_tibble(confint.default(model))
   names(mod_ci) <- c("conf_low", "conf_high")
@@ -21,5 +22,11 @@ ftidy <- function(model, exp = TRUE) {
   out <- cbind(model_tab, mod_ci)
   names(out)[names(out) == "p.value"] <- "p"
   
-  out[c("term", "estimate", "conf_low", "conf_high", "p")]
+  out <- out[c("term", "estimate", "conf_low", "conf_high", "p")]
+  
+  if (drop_intercept) {
+    out[!grepl("Intercept", out$term), ]
+  } else {
+    out
+  }
 }
