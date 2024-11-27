eb26f1d8426191c5aa989085286039dd7398a92d
Alan Yeung <alan.yeung@phs.scot>
2024-11-27 16:50:52 +0000

    more arguments for np_table

--- R/np_table.R
+++ R/np_table.R
@@ -3,14 +3,17 @@
 #'
 #' @param data Input dataset.
 #' @param vars Character vector of variables to get counts and percentages for.
+#' @param acc Number to round to for percentages in the combined count and percentage variable (e.g. 0.1 shows one decimal place for the percentage).
 #' @param np_only Option to return only the combined count and percentage variable.
+#' @param header Option to use variable names as headers.
+#' @param var_col Option to include variable name as a separate column.
 #' @return Data frame with counts and percentages for a list of variables.
 #' @export
 #' @examples
 #' \dontrun{
-#' np_table(dat, c("var1", "var2"))
+#' np_table(dat, c("var1", "var2"), acc = 0.1)
 #' }
-np_table <- function(data, vars, np_only = TRUE) {
+np_table <- function(data, vars, acc = 1, np_only = TRUE, header = TRUE, var_col = FALSE) {
   out <- purrr::map2_df(
     .x = dplyr::syms(vars),
     .y = vars,
@@ -18,15 +21,27 @@ np_table <- function(data, vars, np_only = TRUE) {
         janitor::tabyl(!!{{.x}}) %>% 
         dplyr::rename(category = 1) %>% 
         dplyr::mutate(category = as.character(category)) %>% 
-        dplyr::add_row(category = .y, .before = 1)
+        dplyr::add_row(category = .y, .before = 1) %>% 
+        dplyr::mutate(var = .y, .before = 1)
     }) %>%
-    dplyr::add_row(category = "Total", n = nrow(data), percent = 1, 
-                   .before = 1) %>% 
-    dplyr::mutate(n_perc = np(.data$n, .data$percent))
+    dplyr::add_row(var = "Total", category = "Total", 
+                   n = nrow(data), percent = 1, .before = 1) %>% 
+    dplyr::mutate(n_perc = np(.data$n, .data$percent, acc = acc)) %>% 
+    tibble::as_tibble()
+  
+  if (!header) {
+    out <- dplyr::filter(out, !is.na(.data$n))
+  }
+  
+  out_vars <- names(out)
   
   if (np_only) {
-    tibble::as_tibble(dplyr::select(out, "category", "n_perc"))
-  } else {
-    tibble::as_tibble(dplyr::select(out, "category", "n", "percent", "n_perc"))
+    out_vars <- setdiff(out_vars, c("n", "percent"))
   }
+  
+  if (!var_col) {
+    out_vars <- setdiff(out_vars, "var")
+  }
+  
+  out[out_vars]
 }
