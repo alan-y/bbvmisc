8204a6b749b7459c9b0412ed3fc8225270f25cbd
Alan Yeung <alan.yeung@phs.scot>
2024-11-28 14:24:16 +0000

    comma argument in np function

--- R/np.R
+++ R/np.R
@@ -4,18 +4,23 @@
 #' @param n Counts.
 #' @param p Proportions.
 #' @param digits Number of digits to use after the decimal point in percentages.
+#' @param comma Option to use comma separator on thousands.
 #' @return Counts with proportions in brackets.
 #' @export
 #' @examples
 #' \dontrun{
 #' np(n, p, digits = 1)
 #' }
-np <- function(n, p, digits = 0) {
+np <- function(n, p, digits = 0, comma = FALSE) {
   stopifnot(is.numeric(n) && is.numeric(p))
   
   perc <- janitor::round_half_up(p * 100, digits = digits)
   perc <- formatC(perc, digits = digits, format = "f")
   perc <- paste0(perc, "%")
+  
+  if (comma) {
+    n <- prettyNum(n, big.mark = ",")
+  }
   
   glue::glue("{n} ({perc})", .na = NULL)
 }
