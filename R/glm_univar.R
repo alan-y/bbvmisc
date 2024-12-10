53569b0aa0ef8f795e468e600155311f4ac35f62
meglancy <megan.glancy@gcu.ac.uk>
2024-12-10 11:59:27 +0000

    add param descriptions and update function to pass checks

--- R/glm_univar.R
+++ R/glm_univar.R
@@ -1,17 +1,20 @@
 #' glm_univar
 #' @description Run a univariable `glm()` for a vector of predictor variables with tidy df output
 #' 
-#' @param data 
-#' @param respvar 
-#' @param predvars 
-#' @param family 
-#' @param exp 
-#' @param additional_term 
-#' @param fast 
-#' @return
+#' @param data Input dataset
+#' @param respvar Single character string of response variable
+#' @param predvars Vector of character strings of predictor variables
+#' @param family Single character string of family function
+#' @param exp Option to exponentiate model estimates (default is TRUE)
+#' @param additional_term Option to add additional terms to model (default is blank)
+#' @param fast Option to use normal approximation for confidence intervals (default is TRUE)
+#' @return Regression model table with confidence intervals
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#' glm_univar(dat, "var", c("var1", "var2"), additional_term="+ offset(log(py))")
+#' }
 
 glm_univar <- function(data, respvar, predvars, family = "poisson", exp = TRUE, 
                        additional_term = "", fast = TRUE) {
@@ -38,13 +41,12 @@ glm_univar <- function(data, respvar, predvars, family = "poisson", exp = TRUE,
     ) 
   }
   
-  out <- dplyr::bind_rows(out) %>% 
-    dplyr::filter(term != "(Intercept)") %>% 
-    dplyr::mutate(
-      predvars = stringr::str_match(term, paste0(predvars, collapse = "|")),
-      term = stringr::str_remove(term, paste0(predvars, collapse = "|")),
-      .before = 1
-    )
+  out <- dplyr::bind_rows(out)
+  
+  out[out$term != "(Intercept)", ]
+  out$predvars <- stringr::str_match(out$term, paste0(predvars, collapse = "|"))
+  out$term <- stringr::str_remove(out$term, paste0(predvars, collapse = "|"))
+  out[, c(ncol(out), seq(ncol(out) - 1))]
   
   return(out)
 }
