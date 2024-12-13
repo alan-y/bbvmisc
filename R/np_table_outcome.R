659721229d73741ebe1bac241aed1acdc6e04f94
meglancy <megan.glancy@gcu.ac.uk>
2024-12-13 16:59:54 +0000

    attempt 2 for np_table_outcome

--- R/np_table_outcome.R
+++ R/np_table_outcome.R
@@ -18,26 +18,27 @@
 np_table_outcome <- function(data, outcome, vars, digits=0, np_only = TRUE, var_col = FALSE){
   
   out <- purrr::map2_df(
-    .x=rlang::syms(vars),
+    .x=syms(vars),
     .y=vars,
     .f=~{data %>% 
         filter(!is.na(!!{{.x}})) %>% 
         janitor::tabyl(!!{{.x}}, !!ensym(outcome)) %>% 
-        dplyr::rename(category = 1) %>% 
+        dplyr::rename(category = 1, outcome0 = 2, outcome1 = 3) %>% 
         dplyr::mutate(category = as.character(category)) %>% 
         dplyr::add_row(category = .y, .before = 1) %>% 
         dplyr::mutate(var = .y, .before = 1)
-    }) %>% 
+    }) %>%
     dplyr::add_row(var = "Total", category = "Total",
-                   `0`=nrow(data[data$drd=="0",]), `1`=nrow(data[data$drd=="1",]),
+                   outcome0 = sum(.$outcome0, na.rm=TRUE), 
+                   outcome1 = sum(.$outcome1, na.rm=TRUE),
                    .before = 1) %>%
-    dplyr::mutate(n_perc_outcome = np(.data$`1`, (.data$`1`/(.data$`1`+.data$`0`)), digits=digits)) %>% 
+    mutate(n_perc_outcome = np(.data$outcome1, (.data$outcome1 / (.data$outcome1 + .data$outcome0)), digits=digits)) %>%
     tibble::as_tibble()
   
   out_vars <- names(out)
   
   if (np_only) {
-    out_vars <- setdiff(out_vars, c("1", "0"))
+    out_vars <- setdiff(out_vars, c("outcome0", "outcome1"))
   }
   
   if (!var_col) {
@@ -45,5 +46,7 @@ np_table_outcome <- function(data, outcome, vars, digits=0, np_only = TRUE, var_
   }
   
   return(out[out_vars])
-  
 }
+
+
+
